6c0b185f23c910f77be4be7dd9ec2cd340ea0ce6
bogewang <236598164@qq.com>
2024-07-07 16:19:35 +0800

    销售情况

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="app-container home">
     <el-row :gutter="0">
-      <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log"> 今日销售额1000</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log"> 今日销销售毛利200</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log"> 今日销销售笔数</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="module">
+          <p>
+            <span>
+              <el-text class="mx-1 " size="large" tag="b">¥3000.00</el-text>
+            </span>
+            <el-text class="mx-1" size="small">昨日 2000.23</el-text>
+          </p>
+          <el-text class="mx-1">今日销售额</el-text>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="module">
+          <p>
+          <span>
+            <el-text class="mx-1 " size="large" tag="b">¥3000.00</el-text>
+          </span>
+            <el-text class="mx-1" size="small">昨日 2000.23</el-text>
+          </p>
+          <el-text class="mx-1">今日销售毛利</el-text>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="module">
+          <p>
+            <el-text class="mx-1" size="large" tag="b">100</el-text>
+          </p>
+          <el-text class="mx-1">今日销售笔数</el-text>
+        </el-card>
+      </el-col>
     </el-row>
 
     <el-row>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 销售</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 进货</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 库存查询</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 商品</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 客户</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 供应商</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 应收</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="update-log" > 销售分析</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 销售</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 进货</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 库存查询</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 商品</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 客户</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 供应商</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 应收</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 销售分析</el-card></el-col>
     </el-row>
 
     <el-row>
@@ -23,13 +50,13 @@
           <div>常用功能</div>
           <el-divider/>
           <el-row>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 进货单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 销售单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 库存查询</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 商品利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 销售利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 客户管理</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 商品管理</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 进货单报表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售单报表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 库存查询</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品利润表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售利润表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 客户管理</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品管理</el-card></el-col>
           </el-row>
         </el-card>
       </el-col>
@@ -38,19 +65,18 @@
           <div>汇总信息</div>
           <el-divider/>
           <el-row>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 进货单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 销售单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 库存查询</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 商品利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="update-log" > 销售利润表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 进货单报表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售单报表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 库存查询</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品利润表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售利润表</el-card></el-col>
           </el-row>
-
         </el-card>
       </el-col>
     </el-row>
 
     <el-row :gutter="0">
-      <el-col :xs="24" :sm="24" :md="12" :lg="24"><el-card class="update-log"> 数据概览</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="24"><el-card class="module"> 数据概览</el-card></el-col>
     </el-row>
 
     <el-row>
@@ -85,71 +111,21 @@ const handleTodaySale = () => {
 
 <style scoped lang="scss">
 .home {
-  blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
-  }
-  hr {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0;
-    border-top: 1px solid #eee;
-  }
-  .col-item {
-    margin-bottom: 20px;
-  }
-
-  ul {
-    padding: 0;
-    margin: 0;
-  }
-
-  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-size: 13px;
   color: #676a6c;
   overflow-x: hidden;
 
-  ul {
-    list-style-type: none;
-  }
-
-  h4 {
-    margin-top: 0px;
-  }
-
-  h2 {
-    margin-top: 10px;
-    font-size: 26px;
-    font-weight: 100;
-  }
-
-  p {
-    margin-top: 10px;
-
-    b {
-      font-weight: 700;
-    }
-  }
-
-  .update-log {
+  .module {
     cursor: pointer;
-    ol {
-      display: block;
-      list-style-type: decimal;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      padding-inline-start: 40px;
+    &:hover {
+      background-color: #f8f8f8;
     }
   }
   .el-row {
     margin-bottom: 20px;
   }
-  .el-row:last-child {
-    margin-bottom: 0;
+  b {
+    font-size: 22px;
   }
 }
 </style>
