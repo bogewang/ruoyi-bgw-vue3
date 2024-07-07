bacd4fa5b27b67dd557c694f59894138a1fcf943
bogewang <236598164@qq.com>
2024-07-07 17:19:29 +0800

    各模块

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-container home">
-    <el-row :gutter="0">
+    <el-row>
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="module">
+        <el-card class="module" shadow="hover">
           <p>
             <span>
               <el-text class="mx-1 " size="large" tag="b">¥3000.00</el-text>
@@ -13,7 +13,7 @@
         </el-card>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="module">
+        <el-card class="module" shadow="hover">
           <p>
           <span>
             <el-text class="mx-1 " size="large" tag="b">¥3000.00</el-text>
@@ -24,7 +24,7 @@
         </el-card>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="module">
+        <el-card class="module" shadow="hover">
           <p>
             <el-text class="mx-1" size="large" tag="b">100</el-text>
           </p>
@@ -34,29 +34,117 @@
     </el-row>
 
     <el-row>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 销售</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 进货</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 库存查询</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 商品</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 客户</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 供应商</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 应收</el-card></el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="6"><el-card class="module" > 销售分析</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><Sell /></el-icon>
+          </p>
+          <p class="center-content">
+            销售
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><SoldOut /></el-icon>
+          </p>
+          <p class="center-content">
+            进货
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><TakeawayBox /></el-icon>
+          </p>
+          <p class="center-content">
+            库存
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><Box /></el-icon>
+          </p>
+          <p class="center-content">
+            商品
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><User /></el-icon>
+          </p>
+          <p class="center-content">
+            客户
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><Avatar /></el-icon>
+          </p>
+          <p class="center-content">
+            供应商
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><PieChart /></el-icon>
+          </p>
+          <p class="center-content">
+            销售报表
+          </p>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="6">
+        <el-card class="module" shadow="hover">
+          <p class="center-content">
+            <el-icon><Printer /></el-icon>
+          </p>
+          <p class="center-content">
+            打印模板
+          </p>
+        </el-card>
+      </el-col>
     </el-row>
 
     <el-row>
       <el-col :xs="24" :sm="24" :md="12" :lg="12">
         <el-card>
-          <div>常用功能</div>
-          <el-divider/>
+          <template #header>
+            <div>常用功能</div>
+          </template>
+
           <el-row>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 进货单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 库存查询</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 客户管理</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品管理</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 进货单报表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 销售单报表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 库存查询</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 商品利润表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 销售利润表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 客户管理</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 商品管理</el-card>
+            </el-col>
           </el-row>
         </el-card>
       </el-col>
@@ -65,18 +153,30 @@
           <div>汇总信息</div>
           <el-divider/>
           <el-row>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 进货单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售单报表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 库存查询</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 商品利润表</el-card></el-col>
-            <el-col :xs="24" :sm="24" :md="12" :lg="8"><el-card class="module" > 销售利润表</el-card></el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 进货单报表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 销售单报表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 库存查询</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 商品利润表</el-card>
+            </el-col>
+            <el-col :xs="24" :sm="24" :md="12" :lg="8">
+              <el-card class="module"> 销售利润表</el-card>
+            </el-col>
           </el-row>
         </el-card>
       </el-col>
     </el-row>
 
     <el-row :gutter="0">
-      <el-col :xs="24" :sm="24" :md="12" :lg="24"><el-card class="module"> 数据概览</el-card></el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="24">
+        <el-card class="module"> 数据概览</el-card>
+      </el-col>
     </el-row>
 
     <el-row>
@@ -97,6 +197,8 @@
 </template>
 
 <script setup name="Index">
+import { Sell, SoldOut, TakeawayBox, Box, User, Avatar, PieChart, Printer } from '@element-plus/icons-vue'
+
 const version = ref('4.0.0')
 
 function goTarget(url) {
@@ -117,13 +219,24 @@ const handleTodaySale = () => {
 
   .module {
     cursor: pointer;
+
     &:hover {
       background-color: #f8f8f8;
     }
   }
+
+  .module .center-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 22px;
+  }
+
   .el-row {
     margin-bottom: 20px;
   }
+
   b {
     font-size: 22px;
   }
