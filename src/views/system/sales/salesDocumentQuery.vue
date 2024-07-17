bf02bf6abec0a9150e5b59dd8569f875ffc403db
bogewang <236598164@qq.com>
2024-07-17 21:33:30 +0800

    搜索框

--- src/views/system/sales/salesDocumentQuery.vue
+++ src/views/system/sales/salesDocumentQuery.vue
@@ -1,30 +1,59 @@
 <template>
   <div class="app-container">
-    <el-row :gutter="24">
-      <el-form :inline="true" :model="queryParams" class="demo-form-inline">
-        <el-form-item label="单据号">
-          <el-input v-model="queryParams.billNum" clearable />
-        </el-form-item>
-        <el-form-item label="仓库">
-          <el-select v-model="queryParams.repo" clearable>
-            <el-option v-for="dict in receiptType" :key="dict.value" :label="dict.label" :value="dict.value" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="客户">
-          <el-select v-model="queryParams.customer" clearable>
-            <el-option v-for="dict in customerList" :key="dict.value" :label="dict.label" :value="dict.value" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="操作人">
-          <el-select v-model="queryParams.operator" clearable>
-            <el-option v-for="dict in operatorList" :key="dict.value" :label="dict.label" :value="dict.value" />
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">Query</el-button>
-        </el-form-item>
-      </el-form>
-    </el-row>
+    <el-form :inline="true" :model="queryParams" class="demo-form-inline" label-width="80px" @keyup.enter="searchQuery">
+      <el-row>
+        <el-col :span="18">
+          <el-row>
+            <el-col :span="6">
+              <el-form-item size="large" label="单据号">
+                <el-input v-model="queryParams.billNum" size="large" clearable />
+              </el-form-item>
+            </el-col>
+
+            <el-col :span="6">
+              <el-form-item size="large" label="仓库">
+                <el-select v-model="queryParams.repo" size="large" clearable>
+                  <el-option v-for="dict in receiptType" :key="dict.value" :label="dict.label" :value="dict.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-form-item label="客户">
+                <el-select v-model="queryParams.customer" clearable>
+                  <el-option v-for="dict in customerList" :key="dict.value" :label="dict.label" :value="dict.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-button-group>
+                <el-button>搜索</el-button>
+                <el-button>高级</el-button>
+              </el-button-group>
+            </el-col>
+          </el-row>
+        </el-col>
+
+        <el-col :span="6">
+          <el-button-group>
+            <el-button>便捷收款</el-button>
+            <el-button>审核</el-button>
+            <el-button>取消审核</el-button>
+            <el-button>删除</el-button>
+            <el-button>新增</el-button>
+            <el-button>导出</el-button>
+          </el-button-group>
+        </el-col>
+      </el-row>
+
+      <el-form-item label="操作人">
+        <el-select v-model="queryParams.operator" clearable>
+          <el-option v-for="dict in operatorList" :key="dict.value" :label="dict.label" :value="dict.value" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Query</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -33,6 +62,10 @@ import { reactive } from 'vue'
 import { listRepository } from '@/api/repository/repository.js'
 import { listCustomer } from '@/api/customer/customer.js'
 import { listUser } from '@/api/system/user.js'
+
+const searchQuery = () => {
+  console.log('搜索')
+}
 
 const receiptType = listRepository()
 const customerList = listCustomer()
