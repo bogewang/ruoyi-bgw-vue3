52d033f9663d613983f582239e08307de03bee77
bogewang <236598164@qq.com>
2024-07-21 08:30:51 +0800

    制单人

--- src/views/system/sales/saleAdd.vue
+++ src/views/system/sales/saleAdd.vue
@@ -84,7 +84,9 @@
               <el-icon size="large" style="color: #64ee64; margin-right: 10px"><CirclePlusFilled /></el-icon>
               <el-icon size="large" style="color: red"><RemoveFilled /></el-icon>
             </el-table-column>
-            <el-table-column prop="name" label="商品名称" width="200" />
+            <el-table-column prop="name" label="商品名称" width="200">
+              <el-input @click="handleProductClick"></el-input>
+            </el-table-column>
             <el-table-column prop="unit" label="单位" width="200" />
             <el-table-column prop="cost" label="成本价(元)" width="200" />
             <el-table-column prop="salePrice" label="售价(元)" width="200" />
@@ -102,7 +104,9 @@
 
           <el-col :span="6">
             <el-form-item label="制单人">
-              <el-input></el-input>
+              <el-select v-model="order.maker" clearable>
+                <el-option v-for="dict in employList" :key="dict.id" :label="dict.name" :value="dict.id" />
+              </el-select>
             </el-form-item>
           </el-col>
 
@@ -141,12 +145,18 @@ import { CirclePlusFilled, RemoveFilled } from '@element-plus/icons-vue';
 import { listCustomer } from '@/api/customer/customer.js';
 import { format } from 'date-fns';
 import { ElMessage } from 'element-plus';
+import { useUserStore } from '@/store/modules/user';
+import { storeToRefs } from 'pinia';
 
+//  制单人
+const userStore = useUserStore();
+const { id, name } = storeToRefs(userStore);
 const order = ref({
   orderTime: new Date(),
   deliverDate: new Date(),
   orderNum: 'XSD' + format(new Date(), 'yyyyMMddHHmmss'),
   needDeliver: false,
+  maker: { id: id, name: name },
 });
 
 const customerList = listCustomer();
@@ -163,6 +173,12 @@ const useCustomerAddress = () => {
   order.value.mobile = customerList.find(item => item.id === order.value.customer).mobile;
 };
 const price = ref();
+
+const employList = [
+  { id: 1, name: 'admin' },
+  { id: 2, name: '员工1' },
+  { id: 3, name: '员工2' },
+];
 
 const tableData = ref([{}, {}, {}, {}, {}, {}, {}, {}]);
 </script>
