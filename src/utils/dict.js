9968b4bda2875a0ab9a7e27795fe3121f686b912
bogewang <236598164@qq.com>
2024-07-22 22:29:14 +0800

    关闭驼峰校验

--- src/utils/dict.js
+++ src/utils/dict.js
@@ -1,5 +1,5 @@
-import useDictStore from '@/store/modules/dict'
-import { getDicts } from '@/api/system/dict/data'
+import useDictStore from '@/store/modules/dict';
+import { getDicts } from '@/api/system/dict/data';
 
 /**
  * 获取字典数据
@@ -14,11 +14,16 @@ export function useDict(...args) {
         res.value[dictType] = dicts;
       } else {
         getDicts(dictType).then(resp => {
-          res.value[dictType] = resp.data.map(p => ({ label: p.dictLabel, value: p.dictValue, elTagType: p.listClass, elTagClass: p.cssClass }))
+          res.value[dictType] = resp.map(p => ({
+            label: p.dictLabel,
+            value: p.dictValue,
+            elTagType: p.listClass,
+            elTagClass: p.cssClass,
+          }));
           useDictStore().setDict(dictType, res.value[dictType]);
-        })
+        });
       }
-    })
+    });
     return toRefs(res.value);
-  })()
+  })();
 }
